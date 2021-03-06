647f9a32e700577175d4d42d73920d064d64876a
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2021-03-06 15:09:49 +0100

    Fix formatting.

--- overlay.nix
+++ overlay.nix
@@ -2,13 +2,13 @@
 self: super:
 let
   sources = import ./nix/sources.nix;
-  gitignore = (import sources.gitignore {}).gitignoreSource;
+  gitignore = (import sources.gitignore { }).gitignoreSource;
   hlib = super.haskell.lib;
   lib = super.lib;
   maybeOptimize = x: if optimize then x else hlib.disableOptimization x;
   pwOverrides = selfh: superh: {
     xmobar-proper-weather = maybeOptimize
-      ((superh.callCabal2nix "xmobar-proper-weather" (gitignore ./.) { }));
+      (superh.callCabal2nix "xmobar-proper-weather" (gitignore ./.) { });
   };
 in {
   haskellPackages = super.haskellPackages.override (old: {
